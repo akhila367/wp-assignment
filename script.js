af6f1446c18bc2646e2f60050909486233765179
akhila367 <akhilaakki367@gmail.com>
2026-03-23 12:39:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function validateForm() {
+    let pass = document.getElementById("password").value;
+    let confirm = document.getElementById("confirm").value;
+
+    if (pass !== confirm) {
+        alert("Passwords do not match!");
+        return false;
+    }
+    return true;
+}
